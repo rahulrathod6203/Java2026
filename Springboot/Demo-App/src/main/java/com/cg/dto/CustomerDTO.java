18dbf760ebddfc0ffec6678a40b8d5417bce2ad0
rahulrathod6203 <rahulrathod6203@gmail.com>
2026-04-09 18:37:58 +0530

    CustomerDTO class created

---
+++ Springboot/Demo-App/src/main/java/com/cg/dto/CustomerDTO.java
@@ -0,0 +1,30 @@
+package com.cg.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CustomerDTO {
+	
+private Long id;
+	
+	
+	private String name;
+	
+	
+	private String email;
+	
+	
+	private String number;
+	
+	
+	private String address;
+
+}
